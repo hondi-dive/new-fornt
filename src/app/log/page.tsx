5a4fab06b0177312caf336ebf104e44fbc23e010
choseonghwan91 <choseonghwan91@gmail.com>
2023-08-05 00:10:11 +0900

    add: 공통 컴포넌트 header 추가

--- src/app/log/page.tsx
+++ src/app/log/page.tsx
@@ -1,5 +1,18 @@
+"use client";
+import Header from "@/components/common/Header";
+import { useRouter } from "next/navigation";
+
 export default function Log() {
+  const router = useRouter();
+
+  const handleNextClick = () => {
+    router.push("logDetail");
+  };
+
   return (
-    <div>Log</div>
-  )
+    <div>
+      <Header title="나의로그 작성" onBackClick={() => router.back()} onNextClick={handleNextClick} />
+      Log
+    </div>
+  );
 }
