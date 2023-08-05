b014b063f52d9c8fa6d55b9ced1db231ba292caf
star <chlvkfksquf@gmail.com>
2023-08-06 00:48:04 +0900

    add: bottomNav 제작

---
+++ src/components/common/BottomNav.tsx
@@ -0,0 +1,25 @@
+import HomeIcon from "@/assets/icons/home.svg";
+import MyIcon from "@/assets/icons/my.svg";
+import WriteIcon from "@/assets/icons/write.svg";
+
+export default function BottomNav() {
+  return (
+    <div
+      className="w-full h-20 px-16 flex bg-white justify-between py-6 relative"
+      style={{
+        borderRadius: "50px 50px 0px 0px",
+      }}
+    >
+      <div
+        className="absolute left-[50%] bg-black rounded-[50%] w-[84px] h-[84px] inline-flex justify-center items-center top-0"
+        style={{
+          transform: "translate(-50%, -50%)",
+        }}
+      >
+        <WriteIcon />
+      </div>
+      <HomeIcon />
+      <MyIcon />
+    </div>
+  );
+}
