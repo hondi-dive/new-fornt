66452013a10bef81fcbf2d0f5252edd6d08c6093
star <chlvkfksquf@gmail.com>
2023-08-05 13:06:23 +0900

    add: 기본 레이아웃 설정

---
+++ src/layouts/DefaultLayout.tsx
@@ -0,0 +1,7 @@
+interface DefaultLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function DefaultLayout({ children }: DefaultLayoutProps) {
+  return <div className="mx-auto max-w-[393px]">{children}</div>;
+}
