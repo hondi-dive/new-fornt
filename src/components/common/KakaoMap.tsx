3235939852318391d52b95c57b573e6672913441
star <chlvkfksquf@gmail.com>
2023-08-07 20:26:42 +0900

    modify: kakaomap

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -1,15 +1,11 @@
-"use client";
+'use client';
 
-import React, { useEffect, useRef, useCallback } from "react";
-import Script from "next/script";
-import Head from "next/head";
+import React, { useEffect, useRef, useCallback, useState, ReactElement } from 'react';
+import Script from 'next/script';
+import Head from 'next/head';
 
-import {
-  KakaoInfoWindow,
-  KakaoLatLng,
-  KakaoMap,
-  KakaoMarker,
-} from "@/types/kakao";
+import { KakaoInfoWindow, KakaoLatLng, KakaoMap, KakaoMarker } from '@/types/kakao';
+import Modal from '@/components/common/Modal';
 
 declare global {
   interface Window {
@@ -17,14 +13,11 @@ declare global {
       maps: {
         Map: new (
           container: HTMLDivElement,
-          options: { center: KakaoLatLng; level: number }
+          options: { center: KakaoLatLng; level: number },
         ) => KakaoMap;
         LatLng: new (lat: number, lng: number) => KakaoLatLng;
-        Marker: new (options: { position: KakaoLatLng }) => KakaoMarker;
-        InfoWindow: new (options?: {
-          content?: string;
-          removable?: boolean;
-        }) => KakaoInfoWindow;
+        Marker: new (options: { position: any; map?: any }) => KakaoMarker;
+        InfoWindow: new (options?: { content?: string; removable?: boolean }) => KakaoInfoWindow;
         event: any;
         load: any;
       };
@@ -36,35 +29,118 @@ const NEXT_PUBLIC_KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_APP_KEY;
 
 const KakaoMap = () => {
   const containerRef = useRef<HTMLDivElement>(null);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState<{ title: string; body: null | ReactElement }>({
+    title: '',
+    body: null,
+  });
 
   const initMap = useCallback(() => {
     if (containerRef.current) {
-      const center = new window.kakao.maps.LatLng(37.55, 126.95);
+      const center = new window.kakao.maps.LatLng(33.365216, 126.200269);
       const map = new window.kakao.maps.Map(containerRef.current, {
         center,
         level: 9,
       });
 
+      const positions = [
+        {
+          id: 1,
+          title: '판포포구',
+          content: <div>판포포구</div>,
+          latlng: new window.kakao.maps.LatLng(33.365216, 126.200269),
+        },
+        {
+          id: 2,
+          title: '월령포구',
+          content: <div>월령포구</div>,
+          latlng: new window.kakao.maps.LatLng(33.378558, 126.21632),
+        },
+        {
+          id: 3,
+          title: '범섬',
+          content: <div>범섬</div>,
+          latlng: new window.kakao.maps.LatLng(33.218354, 126.516717),
+        },
+        {
+          id: 4,
+          title: '황우지선녀탕',
+          content: <div>황우지선녀탕</div>,
+          latlng: new window.kakao.maps.LatLng(33.234152, 126.463248),
+        },
+        {
+          id: 5,
+          title: '김녕포구',
+          content: <div>김녕포구</div>,
+          latlng: new window.kakao.maps.LatLng(33.239479, 126.548912),
+        },
+        {
+          id: 6,
+          title: '김녕해변',
+          content: <div>김녕해변</div>,
+          latlng: new window.kakao.maps.LatLng(33.557752, 126.758989),
+        },
+        {
+          id: 7,
+          title: '중문해수욕장',
+          content: <div>중문해수욕장</div>,
+          latlng: new window.kakao.maps.LatLng(33.243064, 126.412131),
+        },
+        {
+          id: 8,
+          title: '함덕해변',
+          content: <div>함덕해변</div>,
+          latlng: new window.kakao.maps.LatLng(33.543495, 126.669673),
+        },
+        {
+          id: 9,
+          title: '이호테우해변',
+          content: <div>이호테우해변</div>,
+          latlng: new window.kakao.maps.LatLng(33.498197, 126.45293),
+        },
+        {
+          id: 10,
+          title: '협재해변',
+          content: <div>협재해변</div>,
+          latlng: new window.kakao.maps.LatLng(33.39439, 126.239582),
+        },
+      ];
+
+      for (let i = 0; i < positions.length; i++) {
+        const marker = new window.kakao.maps.Marker({
+          map: map,
+          position: positions[i].latlng,
+        });
+        const infowindow = new window.kakao.maps.InfoWindow({
+          content: String(positions[i].content),
+        });
+
+        window.kakao.maps.event.addListener(marker, 'click', () => {
+          setShowModal(true);
+          setShowModal(true);
+          setModalContent({ title: positions[i].title, body: positions[i].content });
+          console.log(positions[i].title);
+        });
+
+        window.kakao.maps.event.addListener(
+          marker,
+          'mouseover',
+          makeOverListener(map, marker, infowindow),
+        );
+        window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+
+        new window.kakao.maps.event.addListener(
+          marker,
+          'mouseover',
+          makeOverListener(map, marker, infowindow),
+        );
+        new window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
+      }
+
       const markerPosition = center;
       const marker = new window.kakao.maps.Marker({
         position: markerPosition,
       });
-
-      const infowindow = new window.kakao.maps.InfoWindow({
-        content: "Hello, Kakao Map!", // 인포윈도우 내용 설정
-        removable: true,
-      });
-
-      window.kakao.maps.event.addListener(
-        marker,
-        "mouseover",
-        makeOverListener(map, marker, infowindow)
-      );
-      window.kakao.maps.event.addListener(
-        marker,
-        "mouseout",
-        makeOutListener(infowindow)
-      );
 
       marker.setMap(map);
     }
@@ -76,7 +152,7 @@ const KakaoMap = () => {
         infowindow.open(map, marker);
       };
     },
-    []
+    [],
   );
 
   const makeOutListener = useCallback((infowindow: KakaoInfoWindow) => {
@@ -92,7 +168,7 @@ const KakaoMap = () => {
   }, [initMap]);
 
   return (
-    <React.Fragment>
+    <>
       <Script
         src={`https://dapi.kakao.com/v2/maps/sdk.js?appkey=${NEXT_PUBLIC_KAKAO_KEY}&autoload=false`}
         onLoad={() => window.kakao.maps.load(initMap)}
@@ -101,12 +177,17 @@ const KakaoMap = () => {
         <link rel="preconnect" href="https://dapi.kakao.com" />
         <link rel="dns-prefetch" href="https://dapi.kakao.com" />
       </Head>
-      <div
-        id="map"
-        ref={containerRef}
-        className="shadow-md h-screen -lg:h-96 w-full"
-      />
-    </React.Fragment>
+      <div id="map" ref={containerRef} className="h-screen w-full" />
+      <Modal
+        showModal={showModal}
+        setShowModal={setShowModal}
+        panelClassName="w-full max-w-[345px] rounded-lg bg-white p-6 text-left align-middle bg-opacity-80"
+        titleClassName="text-black text-xl font-semibold mb-2"
+        title={modalContent.title}
+      >
+        {modalContent.body}
+      </Modal>
+    </>
   );
 };
 
