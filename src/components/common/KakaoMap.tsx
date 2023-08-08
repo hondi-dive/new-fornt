10aea331d8ebd3a883c2433327bfa17994193979
star <chlvkfksquf@gmail.com>
2023-08-08 20:42:05 +0900

    add: 스팟 주소 디자인

--- src/components/common/KakaoMap.tsx
+++ src/components/common/KakaoMap.tsx
@@ -1,11 +1,14 @@
 'use client';
 
-import React, { useEffect, useRef, useCallback, useState, ReactElement } from 'react';
+import React, { useEffect, useRef, useCallback, useState } from 'react';
 import Script from 'next/script';
 import Head from 'next/head';
+import Link from 'next/link';
 
 import { KakaoInfoWindow, KakaoLatLng, KakaoMap, KakaoMarker } from '@/types/kakao';
 import Modal from '@/components/common/Modal';
+import MapPin from '@/assets/icons/mapPin.svg';
+import ArrowCircle from '@/assets/icons/arrowCircle.svg';
 
 declare global {
   interface Window {
@@ -30,9 +33,14 @@ const NEXT_PUBLIC_KAKAO_KEY = process.env.NEXT_PUBLIC_KAKAO_APP_KEY;
 const KakaoMap = () => {
   const containerRef = useRef<HTMLDivElement>(null);
   const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState<{ title: string; body: null | ReactElement }>({
+  const [checkedSpot, setCheckedSpot] = useState<{
+    title: string;
+    address: string;
+    id: null | number;
+  }>({
     title: '',
-    body: null,
+    address: '',
+    id: null,
   });
 
   const initMap = useCallback(() => {
@@ -47,61 +55,61 @@ const KakaoMap = () => {
         {
           id: 1,
           title: '판포포구',
-          content: <div>판포포구</div>,
+          address: '제주특별자치도 제주시 한경면 판포리 2877-1',
           latlng: new window.kakao.maps.LatLng(33.365216, 126.200269),
         },
         {
           id: 2,
           title: '월령포구',
-          content: <div>월령포구</div>,
+          address: '제주특별자치도 제주시 특별자치도, 한림읍 월령리 317-1',
           latlng: new window.kakao.maps.LatLng(33.378558, 126.21632),
         },
         {
           id: 3,
           title: '범섬',
-          content: <div>범섬</div>,
+          address: '제주특별자치도 서귀포시 법환동 산1-1',
           latlng: new window.kakao.maps.LatLng(33.218354, 126.516717),
         },
         {
           id: 4,
           title: '황우지선녀탕',
-          content: <div>황우지선녀탕</div>,
+          address: '제주특별자치도 서귀포시 서홍동 795-5',
           latlng: new window.kakao.maps.LatLng(33.234152, 126.463248),
         },
         {
           id: 5,
           title: '김녕포구',
-          content: <div>김녕포구</div>,
+          address: '제주특별자치도 제주시 구좌읍 김녕리 4074-2',
           latlng: new window.kakao.maps.LatLng(33.239479, 126.548912),
         },
         {
           id: 6,
           title: '김녕해변',
-          content: <div>김녕해변</div>,
+          address: '제주특별자치도 제주시 구좌읍 구좌해안로 237',
           latlng: new window.kakao.maps.LatLng(33.557752, 126.758989),
         },
         {
           id: 7,
           title: '중문해수욕장',
-          content: <div>중문해수욕장</div>,
+          address: '제주특별자치도 서귀포시 중문관광로72번길 29-51',
           latlng: new window.kakao.maps.LatLng(33.243064, 126.412131),
         },
         {
           id: 8,
           title: '함덕해변',
-          content: <div>함덕해변</div>,
+          address: '제주특별자치도 제주시 조천읍 조함해안로 525',
           latlng: new window.kakao.maps.LatLng(33.543495, 126.669673),
         },
         {
           id: 9,
           title: '이호테우해변',
-          content: <div>이호테우해변</div>,
+          address: '제주특별자치도 제주시 도리로 20',
           latlng: new window.kakao.maps.LatLng(33.498197, 126.45293),
         },
         {
           id: 10,
           title: '협재해변',
-          content: <div>협재해변</div>,
+          address: '제주특별자치도 제주시 한림읍 한림로 329-10',
           latlng: new window.kakao.maps.LatLng(33.39439, 126.239582),
         },
       ];
@@ -111,30 +119,17 @@ const KakaoMap = () => {
           map: map,
           position: positions[i].latlng,
         });
-        const infowindow = new window.kakao.maps.InfoWindow({
-          content: String(positions[i].content),
-        });
 
         window.kakao.maps.event.addListener(marker, 'click', () => {
           setShowModal(true);
           setShowModal(true);
-          setModalContent({ title: positions[i].title, body: positions[i].content });
+          setCheckedSpot({
+            title: positions[i].title,
+            address: positions[i].address,
+            id: positions[i].id,
+          });
           console.log(positions[i].title);
         });
-
-        window.kakao.maps.event.addListener(
-          marker,
-          'mouseover',
-          makeOverListener(map, marker, infowindow),
-        );
-        window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
-
-        new window.kakao.maps.event.addListener(
-          marker,
-          'mouseover',
-          makeOverListener(map, marker, infowindow),
-        );
-        new window.kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
       }
 
       const markerPosition = center;
@@ -144,21 +139,6 @@ const KakaoMap = () => {
 
       marker.setMap(map);
     }
-  }, []);
-
-  const makeOverListener = useCallback(
-    (map: KakaoMap, marker: KakaoMarker, infowindow: KakaoInfoWindow) => {
-      return function () {
-        infowindow.open(map, marker);
-      };
-    },
-    [],
-  );
-
-  const makeOutListener = useCallback((infowindow: KakaoInfoWindow) => {
-    return function () {
-      infowindow.close();
-    };
   }, []);
 
   useEffect(() => {
@@ -183,9 +163,20 @@ const KakaoMap = () => {
         setShowModal={setShowModal}
         panelClassName="w-full max-w-[345px] rounded-lg bg-white p-6 text-left align-middle bg-opacity-80"
         titleClassName="text-black text-xl font-semibold mb-2"
-        title={modalContent.title}
+        title={checkedSpot.title}
       >
-        {modalContent.body}
+        <div className="flex">
+          <MapPin />
+          <span className="mb-5">{checkedSpot.address}</span>
+        </div>
+        <button className="bg-[#567BFF] py-4 text-white w-[calc(100%-32px)] rounded-lg outline-none ml-4 relative">
+          <Link href={`/feed/${checkedSpot.id}`}>
+            게시판으로 이동하기
+            <div className="absolute right-4 bottom-2">
+              <ArrowCircle />
+            </div>
+          </Link>
+        </button>
       </Modal>
     </>
   );
