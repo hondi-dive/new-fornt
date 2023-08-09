82349e50feee1284ea5cc0bc5bd078f08d5e68a5
star <chlvkfksquf@gmail.com>
2023-08-09 20:08:41 +0900

    modify: SearchInput 공통화

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,18 @@
-import KakaoMap from "@/components/common/KakaoMap";
-import SearchInput from "@/components/page/home/SearchInput";
+import KakaoMap from '@/components/common/KakaoMap';
+import SearchInput from '@/components/common/SearchInput';
 
 export default function Home() {
   return (
     <div className="relative">
       <KakaoMap />
-      <SearchInput />
+      <div
+        className="absolute top-[64px] z-10 left-[50%] w-[calc(100%-24px)]"
+        style={{
+          transform: 'translate(-50%, -50%)',
+        }}
+      >
+        <SearchInput placeholder="원하는 바다를 검색해주세요" />
+      </div>
     </div>
   );
 }
