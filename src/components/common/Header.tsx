c49e22f1094fe010da9f6694b67b5f9d7bf5ee33
star <chlvkfksquf@gmail.com>
2023-08-09 20:09:12 +0900

    modify: header fiexed 부여

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -8,7 +8,7 @@ interface Props {
 
 export default function Header({ onBackClick, onNextClick, title }: Props) {
   return (
-    <header className="flex justify-between items-center h-16 px-6 shadow-[0_0_12px_3px_rgba(56,56,56,0.1)]">
+    <header className="flex justify-between items-center h-16 px-6 shadow-[0_0_12px_3px_rgba(56,56,56,0.1)] fixed max-w-[393px] w-full">
       <div>
         {onBackClick && (
           <button onClick={onBackClick}>
