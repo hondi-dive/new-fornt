8af78411196506983f86b8e66b6ea6f376a262ad
star <chlvkfksquf@gmail.com>
2023-08-11 00:16:27 +0900

    add: useTimeout

---
+++ src/hooks/useTimeout.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useRef } from 'react';
+
+export default function useTimeout(
+  callback: () => void,
+  delay: number | null | undefined,
+): React.MutableRefObject<number | null> {
+  const timeoutRef = useRef<number | null>(null);
+  const savedCallback = useRef<() => void>(callback);
+
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  useEffect(() => {
+    const tick = () => savedCallback.current();
+
+    if (typeof delay === 'number') {
+      timeoutRef.current = window.setTimeout(tick, delay);
+
+      return () => {
+        if (timeoutRef.current) {
+          window.clearTimeout(timeoutRef.current);
+        }
+      };
+    }
+  }, [delay]);
+
+  return timeoutRef;
+}
