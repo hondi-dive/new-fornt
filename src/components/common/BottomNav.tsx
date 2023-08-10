2fc43970c36b572e27bc7bb99986e7ca443da0ec
star <chlvkfksquf@gmail.com>
2023-08-11 00:38:44 +0900

    add: BottomNav 라우팅 연결

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,25 +1,31 @@
-import HomeIcon from "@/assets/icons/home.svg";
-import MyIcon from "@/assets/icons/my.svg";
-import WriteIcon from "@/assets/icons/write.svg";
+import Link from 'next/link';
+
+import HomeIcon from '@/assets/icons/home.svg';
+import MyIcon from '@/assets/icons/my.svg';
+import WriteIcon from '@/assets/icons/write.svg';
 
 export default function BottomNav() {
   return (
     <div
       className="w-full h-20 px-16 flex bg-white justify-between py-6 absolute bottom-0 z-10"
       style={{
-        borderRadius: "50px 50px 0px 0px",
+        borderRadius: '50px 50px 0px 0px',
       }}
     >
       <div
         className="absolute left-[50%] bg-black rounded-[50%] w-[84px] h-[84px] inline-flex justify-center items-center top-0"
         style={{
-          transform: "translate(-50%, -50%)",
+          transform: 'translate(-50%, -50%)',
         }}
       >
         <WriteIcon />
       </div>
-      <HomeIcon />
-      <MyIcon />
+      <Link href="/home">
+        <HomeIcon />
+      </Link>
+      <Link href="/my">
+        <MyIcon />
+      </Link>
     </div>
   );
 }
