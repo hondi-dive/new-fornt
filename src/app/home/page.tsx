1cad4fe0a3768ac39e5ba1dc4f3809d411d85e62
star <chlvkfksquf@gmail.com>
2023-08-11 19:56:21 +0900

    add: 카카오맵 검색 기능

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -3,16 +3,8 @@ import SearchInput from '@/components/common/SearchInput';
 
 export default function Home() {
   return (
-    <div className="relative">
+    <>
       <KakaoMap />
-      <div
-        className="absolute top-[64px] z-10 left-[50%] w-[calc(100%-24px)]"
-        style={{
-          transform: 'translate(-50%, -50%)',
-        }}
-      >
-        <SearchInput placeholder="원하는 바다를 검색해주세요" />
-      </div>
-    </div>
+    </>
   );
 }
