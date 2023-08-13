e10bbd1a928f625d1104927836a2d21c3699bf3f
choseonghwan91 <choseonghwan91@gmail.com>
2023-08-13 20:34:25 +0900

    modify: input 재활용 가능 하도록 변경 - Input 컴포넌트는 input 기능만 담당하도록 수정 - unit을 사용해야 되는 Input 들은 따로 분리

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -2,29 +2,18 @@ import { InputHTMLAttributes } from 'react';
 
 interface Props extends InputHTMLAttributes<HTMLInputElement> {
   _size?: 'medium' | 'small';
-  unit?: string;
 }
 
-export default function Input({ _size = 'medium', unit, ...rest }: Props) {
+export default function Input({ _size = 'medium', ...rest }: Props) {
   const INPUT_SIZE = {
     medium: 'h-[50px]',
     small: 'h-10',
   };
 
   return (
-    <div
-      className={`flex justify-between items-center px-[18px] rounded-lg ${
-        rest.disabled ? 'bg-[#e9eaf4]' : 'border-solid border-[1px] border-[rgb(165,165,165)]'
-      } ${INPUT_SIZE[_size]}`}
-    >
-      <input className=" w-full outline-none disabled:bg-transparent text-sm" {...rest} />
-      {unit && (
-        <span
-          className={`font-medium ml-1 text-sm ${rest.disabled ? 'text-black' : 'text-[#a5a5a5]'}`}
-        >
-          {unit}
-        </span>
-      )}
-    </div>
+    <input
+      className={`w-full outline-none text-sm px-[18px] rounded-lg bg-transparent ${INPUT_SIZE[_size]}`}
+      {...rest}
+    />
   );
 }
