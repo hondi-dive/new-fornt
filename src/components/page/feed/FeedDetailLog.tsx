76f0d15eee49ee79dc719629a7ebee4fd347b320
choseonghwan91 <choseonghwan91@gmail.com>
2023-08-28 23:37:10 +0900

    fix: 로그기록 보러가기 시 있는 데이터만 표시 되도록 수정

--- src/components/page/feed/FeedDetailLog.tsx
+++ src/components/page/feed/FeedDetailLog.tsx
@@ -26,123 +26,153 @@ export default function FeedDetailLog({ feedData, routeFeedDetail }: Props) {
           <IdBadge id={feedData.writer.nickName} />
         </div>
 
-        <div className="mt-10">
-          <LogDataSelector
-            title="입수 형태"
-            value={feedData.approachType || ''}
-            selectedData={[
-              {
-                selectedValue: 'BEATCH',
-                displayValue: '해안',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'BOAT',
-                displayValue: '보트',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'ETC',
-                displayValue: '기타',
-                onClick: () => {},
-              },
-            ]}
-          />
-          <LogDataSelector
-            title="수면해류"
-            value={feedData.surfaceFlow || ''}
-            selectedData={[
-              {
-                selectedValue: 'STRONG',
-                displayValue: '강한해류',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'MIDDLE',
-                displayValue: '중간해류',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'WEAK',
-                displayValue: '약한해류',
-                onClick: () => {},
-              },
-            ]}
-          />
-          <LogDataSelector
-            title="심층해류"
-            value={feedData.deepFlow || ''}
-            selectedData={[
-              {
-                selectedValue: 'STRONG',
-                displayValue: '강한해류',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'MIDDLE',
-                displayValue: '중간해류',
-                onClick: () => {},
-              },
-              {
-                selectedValue: 'WEAK',
-                displayValue: '약한해류',
-                onClick: () => {},
-              },
-            ]}
-          />
-        </div>
+        {(feedData.diveType === 'SCUBA' || feedData.diveType === 'FREEDIVING') && (
+          <div className="mt-10">
+            <LogDataSelector
+              title="입수 형태"
+              value={feedData.approachType || ''}
+              selectedData={[
+                {
+                  selectedValue: 'BEATCH',
+                  displayValue: '해안',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'BOAT',
+                  displayValue: '보트',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'ETC',
+                  displayValue: '기타',
+                  onClick: () => {},
+                },
+              ]}
+            />
+            <LogDataSelector
+              title="수면해류"
+              value={feedData.surfaceFlow || ''}
+              selectedData={[
+                {
+                  selectedValue: 'STRONG',
+                  displayValue: '강한해류',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'MIDDLE',
+                  displayValue: '중간해류',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'WEAK',
+                  displayValue: '약한해류',
+                  onClick: () => {},
+                },
+              ]}
+            />
+            <LogDataSelector
+              title="심층해류"
+              value={feedData.deepFlow || ''}
+              selectedData={[
+                {
+                  selectedValue: 'STRONG',
+                  displayValue: '강한해류',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'MIDDLE',
+                  displayValue: '중간해류',
+                  onClick: () => {},
+                },
+                {
+                  selectedValue: 'WEAK',
+                  displayValue: '약한해류',
+                  onClick: () => {},
+                },
+              ]}
+            />
+          </div>
+        )}
 
         <div className=" mt-10">
-          <div className="flex gap-[17px]">
-            <SubTitleContainer title="수온">
-              <InputWithUnit disabled={true} _size="small" unit={'℃'} value={feedData.waterTemp} />
-            </SubTitleContainer>
-            <SubTitleContainer title="기온">
-              <InputWithUnit disabled={true} _size="small" unit={'℃'} value={feedData.temp} />
-            </SubTitleContainer>
-          </div>
+          {(feedData.diveType === 'SCUBA' || feedData.diveType === 'FREEDIVING') && (
+            <div className="flex gap-[17px]">
+              <SubTitleContainer title="수온">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'℃'}
+                  value={feedData.waterTemp}
+                />
+              </SubTitleContainer>
+              <SubTitleContainer title="기온">
+                <InputWithUnit disabled={true} _size="small" unit={'℃'} value={feedData.temp} />
+              </SubTitleContainer>
+            </div>
+          )}
 
-          <div className="flex gap-[17px]">
-            <SubTitleContainer title="입수전 잔량">
-              <InputWithUnit
-                disabled={true}
-                _size="small"
-                unit={'bar'}
-                value={feedData.beforeTank}
-              />
-            </SubTitleContainer>
-            <SubTitleContainer title="입수후 잔량">
-              <InputWithUnit
-                disabled={true}
-                _size="small"
-                unit={'bar'}
-                value={feedData.afterTank}
-              />
-            </SubTitleContainer>
-          </div>
+          {feedData.diveType === 'SCUBA' && (
+            <div className="flex gap-[17px]">
+              <SubTitleContainer title="입수전 잔량">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'bar'}
+                  value={feedData.beforeTank}
+                />
+              </SubTitleContainer>
+              <SubTitleContainer title="입수후 잔량">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'bar'}
+                  value={feedData.afterTank}
+                />
+              </SubTitleContainer>
+            </div>
+          )}
 
-          <div className="flex gap-[17px]">
-            <SubTitleContainer title="다이브 최고수심">
-              <InputWithUnit disabled={true} _size="small" unit={'m'} value={feedData.diveDepth} />
-            </SubTitleContainer>
-            <SubTitleContainer title="포인트 수심">
-              <InputWithUnit disabled={true} _size="small" unit={'m'} value={feedData.pointDepth} />
-            </SubTitleContainer>
-          </div>
+          {feedData.diveType === 'SCUBA' && (
+            <div className="flex gap-[17px]">
+              <SubTitleContainer title="다이브 최고수심">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'m'}
+                  value={feedData.diveDepth}
+                />
+              </SubTitleContainer>
+              <SubTitleContainer title="포인트 수심">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'m'}
+                  value={feedData.pointDepth}
+                />
+              </SubTitleContainer>
+            </div>
+          )}
 
-          <div className="flex gap-[17px]">
-            <SubTitleContainer title="다이브 시간">
-              <InputWithUnit disabled={true} _size="small" unit={'분'} value={feedData.diveTime} />
-            </SubTitleContainer>
-            <SubTitleContainer title="감압시간">
-              <InputWithUnit
-                disabled={true}
-                _size="small"
-                unit={'분'}
-                value={feedData.decompressionTime}
-              />
-            </SubTitleContainer>
-          </div>
+          {feedData.diveType === 'SCUBA' && (
+            <div className="flex gap-[17px]">
+              <SubTitleContainer title="다이브 시간">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'분'}
+                  value={feedData.diveTime}
+                />
+              </SubTitleContainer>
+              <SubTitleContainer title="감압시간">
+                <InputWithUnit
+                  disabled={true}
+                  _size="small"
+                  unit={'분'}
+                  value={feedData.decompressionTime}
+                />
+              </SubTitleContainer>
+            </div>
+          )}
 
           <div className="flex gap-[17px]">
             <div className="w-full">
