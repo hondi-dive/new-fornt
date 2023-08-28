e72362aa7095e43c1ec705bca3d91f352261b4c6
choseonghwan91 <choseonghwan91@gmail.com>
2023-08-29 04:06:35 +0900

    add: 내 게시물도 공유하기 기능 추가

--- src/components/page/feed/FeedSetting.tsx
+++ src/components/page/feed/FeedSetting.tsx
@@ -1,13 +1,18 @@
 import { fetchDiveLogsDelete, fetchTogglePublic } from '@/apis/log';
 import GearSix from '@/assets/icons/GearSix.svg';
+import { FeedDetailType } from '@/types/feed';
 import { Menu, Transition } from '@headlessui/react';
-import { Fragment } from 'react';
+import Script from 'next/script';
+import { Fragment, memo } from 'react';
 
 interface Props {
   diveLogId: string;
+  feedData: FeedDetailType;
 }
 
-export default function FeedSetting({ diveLogId }: Props) {
+const NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY = process.env.NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY;
+
+const FeedSetting = ({ diveLogId, feedData }: Props) => {
   const fetchDeleteLog = async () => {
     try {
       fetchDiveLogsDelete(diveLogId);
@@ -26,51 +31,97 @@ export default function FeedSetting({ diveLogId }: Props) {
     }
   };
 
+  const sendShare = async () => {
+    window.Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: `${feedData.address} - ${
+          feedData.diveType === 'SCUBA'
+            ? '스쿠버다이빙'
+            : feedData.diveType === 'FREEDIVING'
+            ? '프리다이빙'
+            : '스노클링'
+        }`,
+        description: feedData.review,
+        imageUrl: feedData.imageUrl,
+        link: {
+          mobileWebUrl: 'https://hondidive.site/home',
+          webUrl: 'https://hondidive.site/home',
+        },
+      },
+      social: {
+        likeCount: feedData.likeCnt,
+        commentCount: feedData.commentCnt,
+      },
+    });
+  };
+
+  const kakaoInit = () => {
+    if (!window.Kakao.isInitialized()) window.Kakao.init(NEXT_PUBLIC_KAKAO_JAVASCRIPT_KEY);
+  };
+
   return (
-    <Menu as="div" className="relative inline-block text-left">
-      <div>
-        <Menu.Button>
-          <GearSix />
-        </Menu.Button>
-      </div>
-      <Transition
-        as={Fragment}
-        enter="transition ease-out duration-100"
-        enterFrom="transform opacity-0 scale-95"
-        enterTo="transform opacity-100 scale-100"
-        leave="transition ease-in duration-75"
-        leaveFrom="transform opacity-100 scale-100"
-        leaveTo="transform opacity-0 scale-95"
-      >
-        <Menu.Items className="absolute right-0 mt-2 w-[160px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-1 py-1 ">
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                  } group flex w-[152px] items-center rounded-md px-2 py-2 text-sm`}
-                  onClick={fetchDeleteLog}
-                >
-                  삭제하기
-                </button>
-              )}
-            </Menu.Item>
-            <Menu.Item>
-              {({ active }) => (
-                <button
-                  className={`${
-                    active ? 'bg-blue-500 text-white' : 'text-gray-900'
-                  } group flex w-[152px] items-center rounded-md px-2 py-2 text-sm`}
-                  onClick={fetchChangePublic}
-                >
-                  공개/비공개로 전환
-                </button>
-              )}
-            </Menu.Item>
-          </div>
-        </Menu.Items>
-      </Transition>
-    </Menu>
+    <>
+      <Script src="https://developers.kakao.com/sdk/js/kakao.js" onLoad={kakaoInit} />
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button>
+            <GearSix />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 w-[160px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                    } group flex w-[152px] items-center rounded-md px-2 py-2 text-sm`}
+                    onClick={fetchDeleteLog}
+                  >
+                    삭제하기
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                    } group flex w-[152px] items-center rounded-md px-2 py-2 text-sm`}
+                    onClick={fetchChangePublic}
+                  >
+                    공개/비공개로 전환
+                  </button>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <button
+                    className={`${
+                      active ? 'bg-blue-500 text-white' : 'text-gray-900'
+                    } group flex w-[152px] items-center rounded-md px-2 py-2 text-sm`}
+                    onClick={sendShare}
+                  >
+                    공유하기
+                  </button>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </>
   );
-}
+};
+
+export default memo(FeedSetting);
