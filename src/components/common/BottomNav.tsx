48e5b41c533135497f7dea3ee0c316a121b40a00
star <chlvkfksquf@gmail.com>
2023-08-30 14:56:16 +0900

    modify: bottomNav 변경

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -1,11 +1,21 @@
+'use client';
+
 import Link from 'next/link';
 
 import HomeIcon from '@/assets/icons/home.svg';
 import MyIcon from '@/assets/icons/my.svg';
 import WriteIcon from '@/assets/icons/write.svg';
 import ListIcon from '@/assets/icons/list.svg';
+import ActiveHomeIcon from '@/assets/icons/active_home.svg';
+import ActiveMyIcon from '@/assets/icons/active_my.svg';
+import ActiveListIcon from '@/assets/icons/active_list.svg';
+import { usePathname } from 'next/navigation';
+import path from 'path';
 
 export default function BottomNav() {
+  const pathname = usePathname();
+  console.log(pathname);
+
   return (
     <div className="w-full h-20 px-11 flex bg-white justify-between py-3  bottom-0 z-10 fixed max-w-[393px]">
       <Link
@@ -17,15 +27,9 @@ export default function BottomNav() {
       >
         <WriteIcon />
       </Link>
-      <Link href="/home">
-        <HomeIcon />
-      </Link>
-      <Link href="/feeds">
-        <ListIcon />
-      </Link>
-      <Link href="/my">
-        <MyIcon />
-      </Link>
+      <Link href="/home">{pathname === '/home' ? <ActiveHomeIcon /> : <HomeIcon />}</Link>
+      <Link href="/feeds">{pathname === '/feeds' ? <ActiveListIcon /> : <ListIcon />}</Link>
+      <Link href="/my">{pathname === '/my' ? <ActiveMyIcon /> : <MyIcon />}</Link>
       <div className="h-8 w-8" />
     </div>
   );
