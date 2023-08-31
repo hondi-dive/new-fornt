05a694c3b4b3cdb96c1b1aa020ae5c573ffc8543
choseonghwan91 <choseonghwan91@gmail.com>
2023-09-01 01:14:38 +0900

    fix: 해시태그 연속 입력 오류 수정

--- src/components/page/log/HashTagsInput.tsx
+++ src/components/page/log/HashTagsInput.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from 'react';
 
 interface Props {
   updateHashTags: (value: string[]) => void;
@@ -13,6 +13,37 @@ export default function HashTagsInput({ updateHashTags }: Props) {
     hashRef.current?.scrollTo({ left: hashRef.current.scrollWidth - 100 });
     updateHashTags(hashTags);
   }, [hashTags]);
+
+  const handleKeyUp = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Backspace') {
+      if (hashTag === '') {
+        setHashTags((prev) => prev.slice(0, -1));
+      }
+    } else if (event.key === 'Enter') {
+      const hashTagValue = hashTag.replace('#', '');
+      if (hashTagValue.length >= 1) {
+        setHashTags((prev) => [...prev, hashTagValue]);
+        setHashTag('#');
+      }
+    }
+  };
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const shopValue = event.target.value.split('#');
+    if (shopValue.length >= 3) {
+      setHashTags((prev) => [...prev, shopValue[1]]);
+      setHashTag('#');
+      return;
+    }
+    const doubleWhiteSpaceValue = event.target.value.split('  ');
+    if (doubleWhiteSpaceValue.length >= 2) {
+      setHashTags((prev) => [...prev, doubleWhiteSpaceValue[0].substring(1)]);
+      setHashTag('#');
+      return;
+    }
+
+    setHashTag(event.target.value);
+  };
 
   return (
     <div
@@ -30,35 +61,8 @@ export default function HashTagsInput({ updateHashTags }: Props) {
         className="ml-1 outline-none min-w-[100px] bg-transparent"
         value={hashTag}
         placeholder="#해시태그"
-        onChange={(e) => {
-          const shopValue = e.target.value.split('#');
-          if (shopValue.length >= 3) {
-            setHashTags((prev) => [...prev, shopValue[1]]);
-            setHashTag('#');
-            return;
-          }
-          const doubleWhiteSpaceValue = e.target.value.split('  ');
-          if (doubleWhiteSpaceValue.length >= 2) {
-            setHashTags((prev) => [...prev, doubleWhiteSpaceValue[0].substring(1)]);
-            setHashTag('#');
-            return;
-          }
-
-          setHashTag(e.target.value);
-        }}
-        onKeyDown={(e) => {
-          if (e.key === 'Backspace') {
-            if (hashTag === '') {
-              setHashTags((prev) => prev.slice(0, -1));
-            }
-          } else if (e.key === 'Enter') {
-            const hashTagValue = hashTag.replace('#', '');
-            if (hashTagValue.length >= 1) {
-              setHashTags((prev) => [...prev, hashTagValue]);
-              setHashTag('#');
-            }
-          }
-        }}
+        onChange={handleChange}
+        onKeyUp={handleKeyUp}
       />
     </div>
   );
